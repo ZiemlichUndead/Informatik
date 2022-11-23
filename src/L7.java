badc409737fcf0da231d6349c3fc9744a5b893c0
Steffen_LT <steffen_schlosser@gmx.de>
2022-11-23 19:13:52 +0100

    L7 a2

--- src/L7.java
+++ src/L7.java
@@ -1,60 +1,18 @@
+import java.util.Scanner;
+
 public class L7 {
 
     public static void main(String[] args) {
-        System.out.println(pow(10,5));
-        System.out.println(fak(17));
-        System.out.println(Math.sin(1));
-        System.out.println(sin(1));
-
-
-
-
-
-
+        a2();
     }
 
-    public static double cos(double x)
-    {
-        double zaehler = 1.0;
-        double nenner = 1.0;
-        double summe = 1.0;
-        double summand = 1;
-        for (int i = 2; summand > 1E-15 || summand < -1E-15; i = i + 2)
-        {
-            zaehler = zaehler * x * x * (-1);
-            nenner = nenner * i * (i - 1);
-            summand = zaehler / nenner;
-            summe = summe + summand;
-        }
-        return summe;
-    }
-    public static double sins(double x)
-    {
-        double zaehler = 1.0;
-        double nenner = 1.0;
-        double summe = 1.0;
-        double summand = 1;
-        for (int i = 2; summand > 1E-15 || summand < -1E-15; i = i + 2)
-        {
-            zaehler = zaehler * x * x * (-1);
-            nenner = nenner * i * (i - 1);
-            summand = zaehler / nenner;
-            summe = summe + summand;
-        }
-        return summe;
-    }
     public static double sin(double x){
-
         double summand = 1;
         double summe = 0;
         
         for (int k = 0; summand > 1E-15 || summand < -1E-15; k++) {
-            double multiplikant = pow(-1,k);
-            double zaehler = pow(x,2*k+1);
-            double nenner = fak(2*k+1);
-            summand = multiplikant * (zaehler / nenner);
+            summand = pow(-1,k) * (pow(x,2*k+1) / fak(2*k+1));
             summe += summand;
-
         }
         return summe;
     }
@@ -80,5 +38,66 @@ public class L7 {
             result *= i;
         }
         return result;
+    }
+
+    public static void a2(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Anzahl der Zahlen: ");
+        int amount = sc.nextInt();
+        if(amount <= 0){
+            System.out.println("Fehlermeldung: <= 0");
+            return;
+        }
+        double[] a = new double[amount];
+
+        for (int i = 0; i < amount; i++) {
+            System.out.print("Zahl "+(i+1)+" eingeben: ");
+            a[i] = sc.nextDouble();
+        }
+        System.out.println("Mittelwert: "+mitte(a));
+        System.out.println("Abweichung: "+abweichung(a));
+        System.out.println("Max: "+max(a));
+        System.out.println("Min: "+min(a));
+    }
+    public static double mitte(double[] a){
+        double sum = 0;
+        for (double v : a) {
+            sum+=v;
+        }
+        return sum/a.length;
+    }
+
+    public static double abweichung(double[] a){
+        double mittelwert = mitte(a);
+//        double sum = 0;
+//        for (double v : a) {
+//            sum+= pow(v-mittelwert,2);
+//        }
+//        return Math.sqrt(sum/ a.length);
+
+        double[] b = a.clone();
+        for (int i = 0; i < a.length; i++) {
+            b[i] = pow(a[i]-mittelwert,2);
+        }
+        return Math.sqrt(mitte(b));
+    }
+
+    public static double max(double[] a){
+        double res = a[0];
+        for (double v : a) {
+            if(v > res){
+                res = v;
+            }
+        }
+        return res;
+    }
+    public static double min(double[] a){
+        double res = a[0];
+        for (double v : a) {
+            if(v < res){
+                res = v;
+            }
+        }
+        return res;
     }
 }
